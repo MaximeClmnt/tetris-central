bb3c7691ccae8bd5f2e79902a5208b25aca91829
MaximeClmnt <51072744+MaximeClmnt@users.noreply.github.com>
2020-03-11 13:02:50 +0100

    Update helloserver.js

--- helloserver.js
+++ helloserver.js
@@ -1,6 +1,6 @@
 var server = require('http').createServer( function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("hello, world2\n");
+  res.end("hello, world3\n");
   console.log("Hello");
 });
 server.listen(process.env.PORT || 5000);
