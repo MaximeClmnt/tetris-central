08bafa87dcd1aca848501a484287a4cfb8911da9
MaximeClmnt <51072744+MaximeClmnt@users.noreply.github.com>
2020-03-20 20:04:13 +0100

    Update testsocket.js

--- testsocket.js
+++ testsocket.js
@@ -1,8 +1,9 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/testsocket.html');
 });
 
 http.listen(process.env.PORT || 5000, function(){
