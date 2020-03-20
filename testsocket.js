26484d8f7a426f39a4538a0b6866f6f4108094b3
MaximeClmnt <51072744+MaximeClmnt@users.noreply.github.com>
2020-03-20 19:54:10 +0100

    Update testsocket.js

--- testsocket.js
+++ testsocket.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(5000, function(){
+http.listen(process.env.PORT || 5000, function(){
   console.log('listening on *:5000');
 });
