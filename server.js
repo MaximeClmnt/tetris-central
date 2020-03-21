35d075619b69f987e956d74b77c4713cdd368e75
MaximeClmnt <51072744+MaximeClmnt@users.noreply.github.com>
2020-03-21 19:05:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = '/mobile.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
