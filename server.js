f4f93ac4783f09aaeba63f44ca6a523df5690266
MaximeClmnt <51072744+MaximeClmnt@users.noreply.github.com>
2020-03-21 19:18:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express();
-  server.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+const app = express();
+const server = app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
