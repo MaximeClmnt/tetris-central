7e1fc3c52740f721fd97da6237fb8814bfe686e0
Exon17 <62092452+Exon17@users.noreply.github.com>
2020-03-24 17:24:26 +0100

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -35,3 +35,63 @@ function drawBoard(){
 }
 
 drawBoard();
+
+
+// Les pièces et leur couleur
+
+const PIECES = [
+    [Z,"red"],
+    [S,"green"],
+    [T,"yellow"],
+    [O,"blue"],
+    [L,"purple"],
+    [I,"cyan"],
+    [J,"orange"]
+];
+
+// Générer un pièce aléatoire
+
+function randomPiece(){
+    let r = randomN = Math.floor(Math.random() * PIECES.length) // 0 -> 6
+    return new Piece( PIECES[r][0],PIECES[r][1]);    // forme et couleur ex : [Z,"red"]
+}
+
+let p = randomPiece();
+
+// L'objet pièce
+
+function Piece(forme,color){
+    this.forme = forme;
+    this.color = color;
+    
+    this.formeN = 0; // on commence avec la première forme de la pièce.
+    this.activeforme = this.forme[this.formeN];		// première forme de la pièce (position de base)
+		
+    this.x = 3;			// Toutes les formes sont sur des matrices de 4*4, on lance la pièce à la position x = 3 y = -2
+    this.y = -2;		
+}
+
+// Méthode pour dessiner la pièce.
+
+Piece.prototype.fill = function(color){
+    for( r = 0; r < this.activeforme.length; r++){
+        for(c = 0; c < this.activeforme.length; c++){
+            // on ne dessine que les cases remplies.
+            if( this.activeforme[r][c]){		// activeforme[r][c] = 1 ou 0 ; 1 on remplit la case
+                drawSquare(this.x + c,this.y + r, color);
+            } 
+        }
+    }
+}
+
+// Dessine une pièce sur le canvas
+
+Piece.prototype.draw = function(){
+    this.fill(this.color);
+}
+
+// Efface une pièce sur le canvas
+
+Piece.prototype.unDraw = function(){
+    this.fill(VACANT);
+}
